ef1995403c3a909c28a710f836f346c00828a6c4
ingalls <nick@ingalls.ca>
2025-06-23 10:57:40 -0600

    Initial Version

---
+++ task.ts
@@ -0,0 +1,148 @@
+import Err from '@openaddresses/batch-error';
+import Schema from '@openaddresses/batch-schema';
+import type { TSchema } from '@sinclair/typebox';
+import { Type } from '@sinclair/typebox';
+import ETL, { DataFlowType, SchemaType, handler as internal, local, InvocationType } from '@tak-ps/etl';
+import type { Event } from '@tak-ps/etl';
+
+export interface Share {
+    ShareId: string;
+    CallSign?: string;
+    Password?: string;
+}
+
+const EverywhereItem = Type.Object({
+    converterId: Type.String(),
+    deviceId: Type.Integer(),
+    teamId: Type.Integer(),
+    trackPoint: Type.Object({
+        time: Type.Integer(),
+        direction: Type.Integer(),
+        inboundMessageId: Type.Integer(),
+        isEmergency: Type.Optional(Type.Boolean()),
+        source: Type.Optional(Type.String()),
+        alertsList: Type.Optional(Type.Array(Type.Object({
+            id: Type.Integer(),
+            description: Type.String(),
+            type: Type.String()
+        }))),
+        point: Type.Object({
+            x: Type.Number(),
+            y: Type.Number()
+        }),
+    }),
+    source: Type.String(),
+    entityId: Type.Integer(),
+    deviceType: Type.String(),
+    name: Type.String(),
+    alias: Type.Optional(Type.String())
+})
+
+const Input = Type.Object({
+    'DEBUG': Type.Boolean({
+        default: false,
+        description: 'Print debug info in logs'
+    })
+})
+
+export default class Task extends ETL {
+    static name = 'etl-everywhere-hub'
+    static flow = [ DataFlowType.Incoming ];
+    static invocation = [ InvocationType.Webhook ];
+
+    static async webhooks(
+        schema: Schema,
+        task: Task
+    ): Promise<void> {
+        const env = await task.env(Input);
+
+        schema.post('/:webhookid', {
+            name: 'Incoming Webhook',
+            group: 'Default',
+            description: 'Get an Everywhere Hub InReach Update',
+            params: Type.Object({
+                webhookid: Type.String()
+            }),
+            body: env.DEBUG ? Type.Any() : EverywhereItem,
+            res: Type.Object({
+                status: Type.Number(),
+                message: Type.String()
+            })
+        }, async (req, res) => {
+            if (env.DEBUG) {
+                console.error(`DEBUG Webhook: ${req.params.webhookid} - ${JSON.stringify(req.body, null, 4)}`);
+            }
+
+            try {
+                await task.submit({
+                    type: 'FeatureCollection',
+                    features: [{
+                        id: `inreach-${req.body.deviceId}`,
+                        type: 'Feature',
+                        properties: {
+                            course: req.body.trackPoint.direction,
+                            callsign: req.body.alias || req.body.name,
+                            time: new Date(req.body.trackPoint.time).toISOString(),
+                            start: new Date(req.body.trackPoint.time).toISOString(),
+                            metadata: {
+                                inreachId: req.body.deviceId,
+                                inreachName: req.body.name,
+                                inreachDeviceType: req.body.deviceType,
+                                inreachDeviceId: req.body.deviceId,
+                                inreachReceive: new Date(req.body.trackPoint.time).toISOString()
+                            }
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [ req.body.trackPoint.point.x, req.body.trackPoint.point.y ]
+                        }
+                    }]
+                });
+
+                res.json({
+                    status: 200,
+                    message: 'Received'
+                });
+            } catch (err) {
+                Err.respond(err, res);
+            }
+        })
+    }
+
+    async schema(
+        type: SchemaType = SchemaType.Input,
+        flow: DataFlowType = DataFlowType.Incoming
+    ): Promise<TSchema> {
+        if (flow === DataFlowType.Incoming) {
+            if (type === SchemaType.Input) {
+                return Input;
+            } else {
+                return Type.Object({
+                    inreachId: Type.String(),
+                    inreachName: Type.String(),
+                    inreachDeviceType: Type.String(),
+                    inreachIMEI: Type.Optional(Type.String()),
+                    inreachIncidentId: Type.Optional(Type.String()),
+                    inreachValidFix: Type.Optional(Type.String()),
+                    inreachText: Type.Optional(Type.String()),
+                    inreachEvent: Type.Optional(Type.String()),
+                    inreachDeviceId: Type.String(),
+                    inreachReceive: Type.String({ format: 'date-time' }),
+                })
+            }
+        } else {
+            return Type.Object({});
+        }
+    }
+}
+
+await local(new Task(import.meta.url), import.meta.url);
+
+export async function handler(event: Event = {}, context?: object) {
+    return await internal(new Task(import.meta.url, {
+        logging: {
+            event: process.env.DEBUG ? true : false,
+            webhooks: true
+        }
+    }), event, context);
+}

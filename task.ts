14db63ea16dfbef45e4ca247a1aebd3cbcf3704b
ingalls <nick@ingalls.ca>
2025-07-02 14:33:33 -0600

    Get Ephemeral

--- task.ts
+++ task.ts
@@ -163,6 +163,7 @@ export default class Task extends ETL {
     }
 
     async control(): Promise<void> {
+        const layer = await this.fetchLayer();
         const env = await this.env(Input);
 
         const ephem = await this.ephemeral(EphemeralStore, DataFlowType.Incoming);
